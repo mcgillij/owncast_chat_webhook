2f4b98d23805803c8c7cadf1b87b9527a7177716
mcgillij <mcgillivray.jason@gmail.com>
2021-09-20 21:44:46 -0300

    updated to have a timestamp

--- client.py
+++ client.py
@@ -3,6 +3,16 @@ import json
 
 webhook_url = 'http://localhost:5000/webhook'
 
-data = { 'name': 'jason', 'description': 'some desc' }
+data = {'eventData': {'body': 'f',
+               'id': 'MHLpo7Hng',
+               'rawBody': 'f',
+               'timestamp': '2021-09-20T23:02:54.980066719Z',
+               'user': {'createdAt': '2021-09-20T22:34:33.139297191Z',
+                        'displayColor': 22,
+                        'displayName': 'mcgillij',
+                        'id': 'avyQt7N7R',
+                        'previousNames': ['mcgillij']},
+               'visible': True},
+         'type': 'CHAT'}
 
 r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+from datetime import datetime
+from dateutil import parser
 from flask import Flask, request, abort
-# from pprint import pprint
 
 app = Flask(__name__)
 
@@ -8,22 +9,22 @@ file_to_write_to = '/home/j/obs_stuffs/chatlog.txt'
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        # pprint(request.json)
         message = request.json.get('eventData').get('body')
         user = request.json.get('eventData').get('user').get('displayName')
+        timestamp = request.json.get('eventData').get('timestamp')
+        formatted = datetime.strftime(parser.isoparse(timestamp), '%H:%M:%S')
 
-        if user and message:
-            write_out_chatlog(user, message)
+        if user and message and timestamp:
+            write_out_chatlog(formatted, user, message)
         return 'success', 200
     else:
         abort(400)
 
-def write_out_chatlog(user, message):
+def write_out_chatlog(timestamp, user, message):
     with open(file_to_write_to, mode='a') as chatlog:
-        chatlog.write(f"{user}: {message}\n")
+        chatlog.write(f"{timestamp} {user}: {message}\n")
 
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
